23de4051ca2691a26fd91acdcdd70cd27f7e869d
TejasKarde21 <tejaskarde98@gmail.com>
2023-11-19 09:59:26 +0530

    textEditor

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import 'bootstrap/dist/css/bootstrap.css';
+import TextForm from './TextForm';
+
+function App() {
+ return(
+<>
+<TextForm heading={"Enter Text Here"}/>
+</>
+ ); 
+}
+
+export default App;
